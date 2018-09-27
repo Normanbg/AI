9a0227985cbedf3204848e0adf040b42b9fb58e1
Normanbg <normanbg98@gmail.com>
2018-09-27 19:03:51 +0200

    Rollball finished

---
+++ Rollball/Assets/Scripts/sc.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sc : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 mov = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(mov * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "YOU WIN";
+        }
+    }
+}
